a66d47557c2d60a5d52ff6c7483c1a97ff30210f
Muhamad Ainurohman <ainurohman24@gmail.com>
2024-12-13 14:39:16 +0700

    Create unsigned.cpp
    UNSIGNED C++

---
+++ unsigned.cpp
@@ -0,0 +1,12 @@
+#include <iostream.sh>
+#include <conio.h>
+{
+        unsigned int jumlah, harga;
+        int total;
+        jumlah = 10;
+        harga = 5000; // jumlah >= 0
+        total = jumlah * harga; // harga >=0
+        cout << "Total = " << total;
+        getch();
+        return 0;
+}
