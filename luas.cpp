2bf7a42533ceada00fa06c071057dabe1aaa3338
Muhamad Ainurohman <ainurohman24@gmail.com>
2024-12-13 14:48:08 +0700

    Create luas.cpp
    Program Menghitung LUAS

---
+++ luas.cpp
@@ -0,0 +1,10 @@
+#include <iostream.h>
+#include <conio.h>
+int main () {
+        int panjang = 5, lebar (4);
+        int luas;
+        luas = panjang * lebar;
+        cout << "Luas = " << luas;
+        getch();
+        return 0;
+}      
