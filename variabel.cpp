ca75e02b37a4a61ec97706318ce50f6e53a9b095
Muhamad Ainurohman <ainurohman24@gmail.com>
2024-12-13 14:20:32 +0700

    VARIABEL C++
    Materi variabel C++

---
+++ variabel.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+#include <conio.h>
+
+int main ()
+{
+        // variabel dalam c++
+        int a, b;
+        int result;
+
+        // proses program
+        a = 5;
+        b = 2;
+        result = a / b;
+        cout << "Hasil (int) = " << result;
+        getch();
+
+        return 0;
+}
