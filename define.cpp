ead273c4f10f1d36f900de2c071369527f4add72
Muhamad Ainurohman <ainurohman24@gmail.com>
2024-12-13 15:01:17 +0700

    Create define.cpp
    FUNGSI DEFINE PADA C++

---
+++ define.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h>
+#include <conio.h>
+#define PI 3.14159
+#define NEWLINE '\n'
+int main ()
+{
+        double r=5.0;
+        double circle;
+        circle = 2 * PI * r;
+        cout << circle;
+        cout << NEWLINE;
+        getch();
+        return 0;
+}
