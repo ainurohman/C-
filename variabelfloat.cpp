d5d15baf7d2925d2f1981ef4d3792635eca53792
Muhamad Ainurohman <ainurohman24@gmail.com>
2024-12-13 14:27:35 +0700

    Create variabelfloat.cpp
    Variabel dengan Type Data Float

---
+++ variabelfloat.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+#include <conio.h>
+
+int main ()
+{
+        // variabel dalam c++
+        float a, b;
+        float result;
+
+        // proses program
+        a = 5;
+        b = 2;
+        result = a / b;
+        cout << "Hasil (float) = " << result;
+        getch();
+
+        return 0;
+}
